095fac863fd675872bcb911d93a29ee479ad610b
Anurag Sulkiya <163811905+Anurag-Sulkiya@users.noreply.github.com>
2026-05-07 08:12:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  var table = document.getElementId("sampleTable");
+  var newRow = table.insertRow(0);
+  var leftCell = newRow.insertCell(0);
+  var rightCell = newRow.insertCell(1);
+	  leftCell.innerHTML = "New Cell1";
+	  rightCell.innerHTML = "NewCell2";
 }
